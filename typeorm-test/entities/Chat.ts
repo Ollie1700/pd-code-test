bf17d28d1fd164e600d42c936e6ba0df74e8aa39
Lucas Andrade <lucasandradedeveloper@gmail.com>
2020-07-16 14:23:51 +0100

    setting up entities

--- typeorm-test/entities/Chat.ts
+++ typeorm-test/entities/Chat.ts
@@ -1,8 +1,29 @@
-import { Entity } from 'typeorm'
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm'
+
+import Message from './Message';
+import Profile from './Profile';
 
 @Entity()
-export class Chat {
+export default class Chat {
 
-  // Implement fields here
+  @Column({
+    readonly: true,
+    unique: true,
+  })
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
+  @ManyToMany(type => Profile)
+  @JoinTable()
+  participants: Profile[];
+
+  @OneToMany(type => Message, message => message.chat)
+  messages: Message[];
 }
